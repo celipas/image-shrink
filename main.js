ed803431bcce172156ef5b1438e931bec310d3b5
celipas <celipas@gmail.com>
2020-10-27 21:45:53 -0600

    platform a d e nv setup

--- main.js
+++ main.js
@@ -1,11 +1,36 @@
 const { app, BrowserWindow } = require('electron');
 
+// Set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = process.platform === 'darwin' ? true : false;
+
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     Title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
   });
+
+  mainWindow.loadFile(`./app/index.html`);
 }
 
-app.on('ready', createMainWindow);
+app.on('ready', () => {
+  createMainWindow();
+});
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+app.on('active', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
