5d03eb6a4c70b70cc4138a89c5d4d8abfce9e65c
celipas <celipas@gmail.com>
2020-10-28 07:55:39 -0600

    menu roles

--- main.js
+++ main.js
@@ -35,17 +35,23 @@ app.on('ready', () => {
 });
 
 const menu = [
-  ...(isMac ? [{ role: 'appMenue' }] : []),
+  ...(isMac ? [{ role: 'appMenu' }] : []),
   {
-    label: 'File',
-    submenu: [
-      {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+W',
-        click: () => app.quit(),
-      },
-    ],
+    role: 'fileMenu',
   },
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
 ];
 
 app.on('window-all-closed', () => {
