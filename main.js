21a3c01844e8be6bcd7bc1044d738f8c089317cf
celipas <celipas@gmail.com>
2020-10-29 08:09:10 -0600

    implementing imagemin and plugins

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+const path = require('path');
+const os = require('os');
+const imagemin = require('imagemin');
+const imageminMozjpeg = require('imagemin-mozjpeg');
+const imageminPngquant = require('imagemin-pngquant');
+const slash = require('slash');
+
 const {
   app,
   BrowserWindow,
   Menu,
   globalShortcut,
   ipcMain,
+  shell,
 } = require('electron');
-// const { ipcMain } = require('electron/main');
 
 // Set env
 process.env.NODE_ENV = 'development';
@@ -103,8 +110,29 @@ const menu = [
 ];
 
 ipcMain.on('image:minimize', (e, options) => {
-  console.log(options);
+  options.dest = path.join(os.homedir(), 'imageshrink');
+  shrinkImage(options);
 });
+
+async function shrinkImage({ imgPath, quality, dest }) {
+  try {
+    const pngQuality = quality / 100;
+    const files = await imagemin([slash(imgPath)], {
+      destination: dest,
+      plugins: [
+        imageminMozjpeg({ quality }),
+        imageminPngquant({
+          quality: [pngQuality, pngQuality],
+        }),
+      ],
+    });
+    console.log(files);
+
+    shell.openPath(dest);
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 app.on('window-all-closed', () => {
   if (!isMac) {
